cebbb714ad7695a2b76efe2d213b524523d89eb2
wassal <berkaya.wissal98@gmail.com>
2023-08-23 10:55:10 +0100

    version 1.1
    Signed-off-by: wissal <berkaya.wissal98@gmail.com>
    Signed-off-by: wassal <berkaya.wissal98@gmail.com>

--- src/main/java/com/example/demo/entity/Distributeur.java
+++ src/main/java/com/example/demo/entity/Distributeur.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
 
-import com.example.demo.entity.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
